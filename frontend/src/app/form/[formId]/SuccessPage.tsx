85ea735bbf65687fb4a2ffe1018f07263c3c451e
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-04-14 00:34:04 +0600

    Update SuccessPage component with new design and icon

--- frontend/src/app/form/[formId]/SuccessPage.tsx
+++ frontend/src/app/form/[formId]/SuccessPage.tsx
@@ -1,3 +1,10 @@
+import { BadgeCheckIcon } from "lucide-react";
+
 export default function SuccessPage() {
-  return <div>SuccessPagde</div>;
+  return (
+    <div className="flex items-center justify-center flex-col text-[#3c2a4d] gap-4 p-8">
+      <BadgeCheckIcon className="" size={64} />
+      <h1 className="text-3xl font-bold">Form Submitted</h1>
+    </div>
+  );
 }
