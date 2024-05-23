df24048e898de7cf993d9888a965a231d245bae6
Tasnimul H. Tauhid <heytauhid@outlook.com>
2024-05-24 03:20:48 +0600

    refactor: Add 404 page for handling not found routes

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function Error() {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-[80vh]">
+        <p className="text-4xl text-">404 | Page Not Found</p>
+      </div>
+    </div>
+  );
+}
